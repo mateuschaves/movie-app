956f0d2f9fd0d43b49af01132669444a85b9eb12
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-10 17:04:59 -0300

    play icon

--- src/pages/Film/index.js
+++ src/pages/Film/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import { Image, Title, Year, Description, Gradient } from './styles';
+import { Image, Title, Year, Description, Gradient, Icon } from './styles';
 
 export default function Film() {
   return (
     <Image
       source={{ uri: 'https://abrilveja.files.wordpress.com/2019/07/blogib_o-rei-leao-2019_feat.jpg?quality=70&strip=info&w=1024' }}
-      blurRadius={0.6}
     >
       <Gradient colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 1)']}>
+        <Icon
+          name="play"
+          size={100}
+        />
         <Title>
           Rei Leão
-      </Title>
-
+       </Title>
         <Year>
-          1998
-      </Year>
-
+          2019
+       </Year>
         <Description>
           Em O Rei Leão, Simba (Donald Glover) é um jovem leão cujo destino é se tornar o rei da selva. Entretanto, uma armadilha.
-      </Description>
+       </Description>
       </Gradient>
-
     </Image>
   )
 }
--- src/pages/Film/styles.js
+++ src/pages/Film/styles.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export const Image = styled.ImageBackground`
-  justify-content: flex-end;
-  align-items: flex-start;
   flex: 1;
+  align-items: center;
   width: 100%;
   height: 100%;
 `;
 
 export const Title = styled.Text`
   color: #FFFFFFFF;
-  font-size: 24px;
+  font-size: 30px;
   font-weight: bold;
-  margin-bottom: 15px;
+  margin-bottom: 10px;
 `;
 
 export const Year = styled.Text`
@@ -27,6 +27,7 @@ export const Description = styled.Text`
   color: #FFFFFFFF;
   font-size: 20px;
   font-weight: 500;
+  text-align: left;
 `;
 
 export const Gradient = styled(LinearGradient)`
@@ -34,4 +35,12 @@ export const Gradient = styled(LinearGradient)`
   justify-content: flex-end;
   padding-bottom: 50px;
   padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const Icon = styled(MaterialCommunityIcons)`
+  display: flex;
+  alignSelf: center;
+  color: #8D0000;
+  margin-bottom: 60px;
 `;
